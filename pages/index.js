08e53d704c3d212c7fac6b55bd6ea33b4be8fe0b
Alireza <alirezayoom@gmail.com>
2022-10-18 09:05:20 +0430

    data fetching for static pages

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,16 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 }
 
 export default HomePage;
