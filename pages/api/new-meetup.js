42c8ae388482b15c355c7de2caa1ca60ac558f36
Alireza <alirezayoom@gmail.com>
2022-10-18 10:14:15 +0430

    mongodb installed

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,22 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
     const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://alireza:alireza@cluster0.qafjedp.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+
+    res.status(201).json({ message: "meetup inserted!" });
   }
 }
 
