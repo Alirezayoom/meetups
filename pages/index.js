384e6113de6d98b465bc83dc2580a84161e2a10b
Alireza <alirezayoom@gmail.com>
2022-10-19 15:48:47 +0430

    head added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
+import { Fragment } from "react";
+import Head from "next/head";
+
 import MeetupList from "../components/meetups/MeetupList";
 import { MongoClient } from "mongodb";
 
 function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
 }
 
 // export async function getServerSideProps(context) {
